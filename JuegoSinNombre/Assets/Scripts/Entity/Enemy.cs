5409147e7217ac1ee944dd379199e744ad4076b2
mrubio98 <mrubio_98@hotmail.com>
2022-11-11 15:46:57 -0300

    MR_archerEnemy

--- JuegoSinNombre/Assets/Scripts/Entity/Enemy.cs
+++ JuegoSinNombre/Assets/Scripts/Entity/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public abstract class Enemy : Entity
 {
     [SerializeField] protected float _attackCooldown;
-    [SerializeField] protected float _cooldownTimer;
+    protected float _cooldownTimer;
     [SerializeField] protected float _attackDistance;
-    [SerializeField] protected float _checkDistancePlayer;
+    protected float _checkDistancePlayer;
 }
--- JuegoSinNombre/Assets/Scripts/Entity/EnemyPatrol.cs
+++ JuegoSinNombre/Assets/Scripts/Entity/EnemyPatrol.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public abstract class EnemyPatrol : Enemy
 {
+    [SerializeField] protected float _waitTime;
+    [SerializeField] protected Transform _wayPoints;
+
     protected abstract void Patrol();
     protected abstract void WaitPatrol();
 }
