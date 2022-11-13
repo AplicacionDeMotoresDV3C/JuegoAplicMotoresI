7d85b94a9fa6479e44b3133afecb09bfaf803502
GuidoGabrielePeruilh <guidogabrielep@gmail.com>
2022-11-13 12:31:58 -0300

    Interfaz Interactuable y puerta
    Por ahora hay una puerta que se activa y se desactiva utilizando la letra E. Toma la interfaz del interactuable

---
+++ JuegoSinNombre/Assets/Scripts/Interactuable/Switch.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour, IInteractuable
+{
+    [SerializeField] float timer;
+    [SerializeField] float timeLimit;
+    [SerializeField] float timeToLimit;
+    private bool doorIsOpen = false;
+    public GameObject door = null;
+
+    private void Update()
+    {
+        Activate();
+        Debug.Log(doorIsOpen);
+    }
+
+    public void Activate()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Apreto E");
+            if (!doorIsOpen)
+            {
+                doorIsOpen = true;
+                door.SetActive(false);
+                Debug.Log("Abro");
+            }
+            else
+            {
+                doorIsOpen = false;
+                door.SetActive(true);
+                Debug.Log("cierro");
+            }
+        }
+    }
+}
