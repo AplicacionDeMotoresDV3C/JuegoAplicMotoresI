82a37315bbb1ff534216eeb01f7cad35c6174ef7
Zebado <sbasualdo.antares@gmail.com>
2022-11-15 16:40:31 -0300

    SB_Player

--- JuegoSinNombre/Assets/Scripts/Player/Player.cs
+++ JuegoSinNombre/Assets/Scripts/Player/Player.cs
@@ -8,8 +8,25 @@ public class Player : Entity
     Vector3 posPj;
     [SerializeField] InteractionDetector _interactable;
     public GameManager myGameManager;
-    float movementX;
+    Rigidbody2D _rb;
+    float _movement;
+    float _baseGravity;
 
+    [SerializeField] float _rollTime = 0.2f;
+    [SerializeField] float _rollForce = 20f;
+    [SerializeField] float _cooldown = 1f;
+    bool _isRoll;
+    bool _CanRoll;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _baseGravity = _rb.gravityScale;
+    }
+    private void Update()
+    {
+
+    }
     void FixedUpdate()
     {
         Move(posPj);
@@ -23,7 +40,7 @@ public class Player : Entity
         
         posPj = transform.position;
 
-        posPj += new Vector3(movementX, 0f, 0) * speed * Time.deltaTime;
+        posPj += new Vector3(_movement, 0f, 0) * speed * Time.deltaTime;
 
         transform.position = posPj;
        
@@ -33,7 +50,7 @@ public class Player : Entity
     {
 
         posPj = Vector3.zero;
-        movementX = Input.GetAxisRaw("Horizontal");
+        _movement = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetKey(KeyCode.E) && _interactable._interatablesInRange.Count > 0)
         {
@@ -43,6 +60,22 @@ public class Player : Entity
         {
             myGameManager.LoadPosition();
         }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(Roll());
+        }
+    }
+    IEnumerator Roll()
+    {
+        _isRoll = true;
+        _CanRoll = false;
+        _rb.gravityScale = 0f;
+        _rb.velocity = new Vector2(_movement * _rollForce, 0f);
+        yield return new WaitForSeconds(_rollTime);
+        _isRoll = false;
+        _rb.gravityScale = _baseGravity;
+        yield return new WaitForSeconds(_cooldown);
+        _CanRoll = true;
     }
 
 
