735f7b878a1edafeadbd46de9e5db06aef3cbe33
GuidoGabrielePeruilh <guidogabrielep@gmail.com>
2022-11-20 18:59:34 -0300

    Update GameManager.cs

--- JuegoSinNombre/Assets/Scripts/Managers/GameManager.cs
+++ JuegoSinNombre/Assets/Scripts/Managers/GameManager.cs
@@ -5,21 +5,61 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 playerPosition;
+    public Vector3 checkpointPlayerPosition;
+
+    bool _isPause = false;
 
     private void Start()
     {
-        playerPosition = player.transform.position;
+        checkpointPlayerPosition = player.transform.position;
     }
 
+
+    #region PLAYER
     public void SavePosition()
     {
-        playerPosition = player.transform.position;
+        checkpointPlayerPosition = player.transform.position;
     }
 
     public void LoadPosition()
     {
-        player.transform.position = playerPosition;
+        player.transform.position = checkpointPlayerPosition;
+    }
+    #endregion
+
+    #region GAMEPLAY_UI
+    public void Pause()
+    {
+        _isPause = true;
+        Time.timeScale = 0;
     }
 
+    public void Resume()
+    {
+        _isPause = false;
+        Time.timeScale = 1;
+    }
+    #endregion
+
+    #region GAMEPLAY_FLOW
+    public void NewGame()
+    {
+
+    }
+
+    public void NextLevel()
+    {
+
+    }
+
+    public void BackToMainMenu()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    #endregion
 }
