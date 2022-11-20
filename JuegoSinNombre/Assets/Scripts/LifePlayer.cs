41c11c011eea8fa3926881a94669106b29519760
Zebado <sbasualdo.antares@gmail.com>
2022-11-20 18:56:40 -0300

    SB_Canvas

--- JuegoSinNombre/Assets/Scripts/LifePlayer.cs
+++ JuegoSinNombre/Assets/Scripts/LifePlayer.cs
@@ -6,18 +6,5 @@ using UnityEngine.Video;
 
 public class LifePlayer : MonoBehaviour
 {
-    public Image _lifeBar;
-    float _life = 100;
-    [SerializeField] Entity _entity;
-
-    private void Start()
-    {
-        _entity = GetComponent<Entity>();
-    }
-    void Update()
-    {
-        _life = Mathf.Clamp(_life, 0, 100);
-
-        _lifeBar.fillAmount = _life / 100;
-    }
+   
 }
