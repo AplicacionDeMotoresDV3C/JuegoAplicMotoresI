be42f5cd729a32ebe29e7d86ed712127b4c76d9a
GuidoGabrielePeruilh <guidogabrielep@gmail.com>
2022-11-28 16:20:58 -0300

    Move en enemy
    Ahora el MOVE esta en el enemy y no en los enemigos particularmente

--- JuegoSinNombre/Assets/Scripts/Entity/Enemy.cs
+++ JuegoSinNombre/Assets/Scripts/Entity/Enemy.cs
@@ -9,4 +9,20 @@ public abstract class Enemy : Entity
     [SerializeField] protected float _attackDistance;
     protected float _checkDistancePlayer;
     protected Player _player;
+
+    protected override void Move(Vector2 direction)
+    {
+        direction.Normalize();
+        direction.x = direction.x * speed;
+        direction.y = _rb.velocity.y;
+
+        _rb.velocity = direction;
+
+        if (direction.x < 0 && transform.localScale.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        if (direction.x > 0 && transform.localScale.x < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+
+        myAnim.MoveAnimation(direction.x * direction.x);
+    }
 }
--- JuegoSinNombre/Assets/Scripts/Entity/EnemyRanged.cs
+++ JuegoSinNombre/Assets/Scripts/Entity/EnemyRanged.cs
@@ -31,22 +31,6 @@ public class EnemyRanged : EnemyPatrol
         else
             Patrol();
     }
-    protected override void Move(Vector2 direction)
-    {
-        direction.Normalize();
-
-        direction.x = direction.x * speed;
-        direction.y = _rb.velocity.y;
-
-        _rb.velocity = direction;
-
-        if (direction.x < 0 && transform.localScale.x > 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-        if(direction.x > 0 && transform.localScale.x < 0)
-            transform.localScale = new Vector3(1, 1, 1);
-
-        myAnim.MoveAnimation(direction.x * direction.x);
-    }
 
     protected override void Attack()
     {
