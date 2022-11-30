8bc69709ea7d767a44ae5e448e15e862cdca043f
Zebado <sbasualdo.antares@gmail.com>
2022-11-30 10:27:01 -0300

    SB_Retoques
    Enemigo

--- JuegoSinNombre/Assets/Scripts/Entity/MeleeEnemy.cs
+++ JuegoSinNombre/Assets/Scripts/Entity/MeleeEnemy.cs
@@ -8,21 +8,38 @@ public class MeleeEnemy : EnemyPatrol
 
     [SerializeField] AnimManager _animManager;
     Vector2 _direction;
+    [SerializeField] GameObject _damaging;
+
 
     private void Start()
     {
+        _player = GameManager.Instance.player.GetComponent<Player>();
 
+        myAnim.SetEvent("AttackHeatBoxPlay", AttackHeatBoxPlay);
+        myAnim.SetEvent("AttackHeatBoxExit", AttackHeatBoxExit);
     }
     private void Update()
     {
-        Move(_direction);
-        if (_isWaiting)
+        _checkDistancePlayer = Vector2.Distance(transform.position, _player.transform.position);
+
+        if (_checkDistancePlayer <= _attackDistance)
+            Attack();
+        else if (_isWaiting)
             WaitPatrol();
         else
             Patrol();
     }
     protected override void Attack()
     {
-    
+        Move(Vector2.zero);
+        myAnim.AttackAnimation();
+    }
+    void AttackHeatBoxPlay()
+    {
+        _damaging.SetActive(true);
+    }
+    void AttackHeatBoxExit()
+    {
+        _damaging.SetActive(false);
     }
 }
