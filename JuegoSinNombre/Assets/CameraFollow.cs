16bd20faf7ce80d876b83a73de4058b42ff7d740
Zebado <sbasualdo.antares@gmail.com>
2022-12-01 16:05:04 -0300

    camera

---
+++ JuegoSinNombre/Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject _player;
+    [SerializeField] float _moveRight;
+    [SerializeField] float _smoothing;
+    Vector3 _playerPos;
+
+    private void Update()
+    {
+        _playerPos = new Vector3(_player.transform.position.x, _player.transform.position.y, transform.position.z);
+
+        if (_player.transform.localScale.x == 1)
+        {
+            _playerPos = new Vector3(_playerPos.x + _moveRight, _playerPos.y, transform.position.z);
+        }
+        else if (_player.transform.localScale.x == -1)
+        {
+            _playerPos = new Vector3(_playerPos.x - _moveRight, _playerPos.y, transform.position.z);
+        }
+        transform.position = Vector3.Lerp(transform.position, _playerPos, _smoothing * Time.deltaTime);
+    }
+}
